fc81119eb9a924a6c182cd2f1fe4a168cc5505e3
MUHAMMAD AQIL FARRUKH <117354912+qiqioberon@users.noreply.github.com>
2024-03-13 22:06:11 +0700

    Update signup.tsx

--- src/api/User/signup.tsx
+++ src/api/User/signup.tsx
@@ -1,21 +1,45 @@
 import { useMutation, MutationFunction } from "@tanstack/react-query";
 import main from "../main";
-import { SignUp as SignUpForm } from "@/types/SignUpForm";
+import { LoginUser as LoginUserType } from "@/types/LoginUserType";
 import { toast } from "react-toastify";
+import useUserStore from "@/store/userStore";
 
-export default function SignUpUser() {
+
+export default function LoginUser() {
 	const {
-		mutate: mutateSignUp,
-		data: responseSignUp,
+		mutate: mutateUseLoginUser,
+		data: response,
 		isSuccess,
 	} = useMutation({
-		mutationFn: (data: SignUpForm) => {
-			const SignUpUser = data;
-			return main.post(`user`, SignUpUser);
+		mutationFn: (data: LoginUserType) => {
+			const loginUser = data;
+			return main.post(`user/login`, loginUser);
 		},
-		onError: (error: Error) => toast.error(error.message.toString()),
-		onSuccess: () => toast.success("Sukses Sign Up"),
+		onError: (error: Error) => toast.error(getErrorMessage(error)),
+		onSuccess: () => toast.success("Sukses Login"),
 	});
 
-	return { mutateSignUp, responseSignUp, isSuccess };
+	if (response && response.data && response.data.accessToken) {
+		const { accessToken } = response.data;
+		useUserStore.getState().setAccessToken(accessToken);
+		localStorage.setItem("accessToken", accessToken);
+		console.log(useUserStore.getState().accessToken);
+	}
+
+
+	return { mutateUseLoginUser, response, isSuccess };
 }
+
+
+function getErrorMessage(error: any) {
+	let resultMessage = "";
+	if (error) {
+		console.log(error.request);
+		const responseJson = JSON.parse(error.request.response);
+		console.log(responseJson.resultMessage.en);
+
+		resultMessage = responseJson.resultMessage.en.toString();
+	}
+	return resultMessage;
+}
+
